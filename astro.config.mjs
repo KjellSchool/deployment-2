a7dbeef455e44f0bd9d7fe7cba77b391539af2e4
elslander <kjell.elslander@student.howest.be>
2025-11-19 11:13:45 +0100

    test 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://your-github-username.github.io",
-  base: "/cck/",
+  site: "https://kjellschool.github.io",
+  base: "/deployment-2/",
 });
